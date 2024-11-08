98d92f0a1056e1fe72188ef1fef0caef9470b874
PowerCex <admin@powercex.com>
2024-11-08 19:56:29 +0100

    add powercex image
    Signed-off-by: PowerCex <admin@powercex.com>

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # Pulling the required Docker images
 docker compose pull
-docker pull hummingbot/hummingbot:latest
+docker pull powercex/hummingbot:latest
 
 # Creating .env file with the required environment variables
 echo "CONFIG_PASSWORD=a" > .env
